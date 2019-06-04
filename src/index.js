a6723609bec91b5ff876c39be9206b09cf40755e
Daniel Fernando Guimarães <danifgr@gmail.com>
2019-06-04 17:06:43 -0400

    improvement commit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 import * as firebase from 'firebase'
 import { BrowserRouter } from 'react-router-dom'
 
@@ -22,5 +21,3 @@ ReactDOM.render(<BrowserRouter>
                     <App />
                 </BrowserRouter>
                 , document.getElementById('root'));
-
-serviceWorker.unregister();
